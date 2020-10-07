b42cd707dca0f98293d125e8eac6ce7ea1939be2
Ekwuno Obinna <obinnacodes@gmail.com>
2020-10-07 14:16:23 +0100

    Updated run command

--- gatsby-project/gatsby-config.js
+++ gatsby-project/gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+      }, 
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
