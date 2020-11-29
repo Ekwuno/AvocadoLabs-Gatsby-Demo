f2d56d6a6a22b1365048fe8ea1d87c98d974363e
Ekwuno Obinna <obinnacodes@gmail.com>
2020-11-29 17:19:35 +0400

    update name

--- gatsby-project/gatsby-config.js
+++ gatsby-project/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Avocado Labs demo site`,
+    title: `Silly site - All the things I miss`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
